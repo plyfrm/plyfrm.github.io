28f17674f61e8daa77914fffdf9e80e39b74362d
marie <marie.retroposon@protonmail.com>
2023-08-31 17:51:43 +0200

    t

--- scripts/index_link.js
+++ scripts/index_link.js
@@ -3,7 +3,7 @@ let not_in_frame = window === window.parent;
 if (not_in_frame) {
 	let index_link = document.getElementById("index_link");
 	let a = document.createElement("a");
-	a.setAttribute("href", "./index.html");
+	a.setAttribute("href", "/index.html");
 	a.innerHTML = "&lt;&lt; Return to INDEX";
 	index_link.append(a);
 	index_link.append(document.createElement("br"));
